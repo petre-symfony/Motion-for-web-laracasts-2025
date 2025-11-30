d6ce33eded19e4a96bfe7530aeb6be4e727ed2c2
petrero <petrero@gmail.com>
2025-11-30 19:09:48 +0200

    2.1 Motion For The web  Reduced Motion Preference: CSS Media Query  - We wanna be good citizens and make responsible use of motion. So let's take a look at the strategies we have to do. So, All right, check this out. I am here on the web.dev website from the Chrome team on the animation and motion page. And if I scroll down, it states that by the age of 40, over 35% of adults will have experienced some sort of vestibular dysfunction. So this can be disease spells, migraines, vertigo, or even permanent vestibular disability. This is not great. So that doesn't mean that 35% of the population over 40 will be affected by your emotions on your website, but technically this portion of people, 35% of 40 plus might get affected. They're exposed to the possibility that it will affect them. So that's a whole lot of people and there's also younger people that get affected by this. So we need to do the right thing and be a good citizen.  It would be a shame to throw away all these animations to honor and respect these accessibility concerns. But there's a way that you can let the user be in charge and decide whether they want animations or not.

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ $title ?? env('APP_NAME') }}</title>
+  @php
+    $cssPath = 'resources/css/' . ($stylesheet ?? 'app.css');
+    $jsPath = 'resources/js/' . ($script ?? 'app.js');
+  @endphp
+
+  @vite(['resources/css/app.css', $cssPath, $jsPath])
+</head>
+
+<body>
+{{ $slot }}
+</body>
+
+</html>
---
+++ resources/views/intro.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="Intro" stylesheet="intro.css">
+  <div class="wrapper">
+    <div class="square"></div>
+  </div>
+</x-layout>
