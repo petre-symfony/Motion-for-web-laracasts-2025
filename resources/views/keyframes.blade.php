e90632b8ede7d527da3a76e1ed991b858578e142
petrero <petrero@gmail.com>
2025-12-08 16:22:19 +0200

    10.1 Keyframe Animations  Let's now dive into CSS key frame animations, which give us a lot more control over what happens between the start and end state of animation. Once again, we're going to start with the basic square and let's jump into the code. All we've got so far is the style for the square. And so we are going to define our very first key frames. You can do so with an AT rule at key frames, and you need to give it a unique name. So let's go with move here. Let's try to move as square to the right by say 300 pixels for the animation. So we want to go from translate x zero to translate x 300 pixels within key frames. You can define specific CSS state at a given step in time. So we can go from, and here we wanna transform translate x zero. So this is our starting point. And then we can go to and there transform translate X 300 pixels.

---
+++ resources/views/keyframes.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="Keyframe Animations" stylesheet="keyframes.css">
+  <div class="wrapper">
+    <div class="square"></div>
+  </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'intro');
 Route::view('/css-transitions', 'css-transitions');
 Route::view('/transition-behaviour', 'transition-behaviour');
+Route::view('/keyframes', 'keyframes');
