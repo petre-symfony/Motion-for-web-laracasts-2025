d42ba19d900b9c51340bf9c6636d347a55048c70
petrero <petrero@gmail.com>
2025-12-08 13:58:16 +0200

    9.1 Transitioning on Enter and Leave  Let's take a look at how we can take care of enter and leave transitions, thanks to relatively recent additions to CSS. I've created a new demo for that one and I'll walk you through the code. So we have this make it pop button and it's going to open a popover when I click on it. Nice. And I can click outside to dismiss the popover.  So here's the markup for it. And by the way, there is no JavaScript at all here. It's using the HTML popover attribute. And if I remove this here, you will see that the popover is no longer a popover. It's just a bit of text and if I click on the button, nothing happens. So by adding the popover attribute here and then linking the ID of the popover with the popover target on the button, we have a fully functional popover element and the browser is even going to add some stars to place it in the center of the page, which is pretty cool. I won't go into any more details than that about popover elements. This is not what this lesson is for, but just know I have a RAB bit on HTML popovers if you're curious to learn more about that.  So here's my button stars. Uh, maybe a quick sidetrack because it's pretty cool. You can see I have this button color CSS variable that I use for the color. And then for the hover state, I use the color mix function and I mix 80% of that blue with 20% of black. So think about it, if you have 80% of blue paint like this button and you add 20% of black, it should make it a little bit darker. And so on a hover we have this 20% black mixed with the blue. Uh, color mix is pretty cool. Alright, so those are the styles for the button. And now let's take a look at the popover. So we have some basic styles like padding with uh, border radius and box shadow on the popover element itself. And then we also style the backdrop, which is the element that goes between the popover and the rest of the page. And here it's a half black, half transparent, uh, overlay.

---
+++ resources/views/transition-behaviour.blade.php
@@ -0,0 +1,11 @@
+<x-layout title="Transition Behavior & Starting Styles" stylesheet="transition-behaviour.css">
+
+  {{-- Popover Trigger --}}
+  <button popovertarget="popover">Make it POP!</button>
+
+  {{-- Popover --}}
+  <div popover id="popover" class="popover">
+    <h2>What's POP-ing?</h2>
+  </div>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'intro');
 Route::view('/css-transitions', 'css-transitions');
+Route::view('/transition-behaviour', 'transition-behaviour');
