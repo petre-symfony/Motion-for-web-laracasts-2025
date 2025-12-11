5086a032f02583cbe52693a0f2c205895b3348cc
petrero <petrero@gmail.com>
2025-12-11 16:39:57 +0200

    12.1 Let's take a look at a couple more animation properties, including one that can be quite confusing. All right, new example. This time you get four squares instead of one. And I'm going to play the animation. It's not going to look great just yet, but this is the whole purpose and the properties we're going to add are going to help us here.  So let's take a look. And as you can see, nice fun animation. Uh, it's called Tadaaa and the squares are TAing into the screen, but then they yank back to their original position. Let's take a look at it one more time, little rotation and then snap back in position. And just like in our previous example, the problem is that we start and end in a different position and then the animation just jumps back to the start.

--- resources/views/keyframes.blade.php
+++ resources/views/keyframes.blade.php
@@ -1,5 +1,9 @@
 <x-layout title="Keyframe Animations" stylesheet="keyframes.css">
-  <div class="wrapper">
-    <div class="square"></div>
+  <div class="wrapper flex">
+    <!--<div class="square"></div> -->
+    <div class="square square--1"></div>
+    <div class="square square--2"></div>
+    <div class="square square--3"></div>
+    <div class="square square--4"></div>
   </div>
 </x-layout>
