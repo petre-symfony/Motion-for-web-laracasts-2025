86fd4e9d9c703a8ccc93b67f589ac66417b75c6d
petrero <petrero@gmail.com>
2025-12-11 19:43:14 +0200

    14.1 The next thing I wanna show you is how you can connect the progress of the animation to the scroll position of the page. So this one is not fully supported across all major browsers just yet, but it's coming very soon and I really think you should know about it.  So I have here an example with a bunch of shapes on the page and as I scroll you will see that there are a lot more shapes. And what we wanna do is to have some sort of enter animation here where the shape sort of scale up and fade in as they enter. So here's the CSS file for it. You can see the different shapes with the stars.

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::view('/css-transitions', 'css-transitions');
 Route::view('/transition-behaviour', 'transition-behaviour');
 Route::view('/keyframes', 'keyframes');
 Route::view('/path', 'path');
+Route::view('/timeline', 'timeline');
