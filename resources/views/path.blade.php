79db7fbfec4e45e77d6cdd39c3e0f04ca5bbc506
petrero <petrero@gmail.com>
2025-12-11 17:56:08 +0200

    13.4 Uh, it's a bit confusing without the actual shape behind. You can actually see that it seems similar, but why don't we actually display the SVG and then try to line it up with the motion that we've created. So in the page template here, let's paste the actual SVG that we copied from Figma. And it's still not going to be great because we're going to have the SVG elements and then below it we're going to have a rectangle that follows the same shape. And so as you'll see, uh, the path is being followed but the elements are stacked on top of each other. So it's not really doing the trick.

--- resources/views/path.blade.php
+++ resources/views/path.blade.php
@@ -1,3 +1,10 @@
 <x-layout title="Motion Path" stylesheet="path.css">
+  <svg width="645" height="327" viewBox="0 0 645 327" fill="none"
+       xmlns="http://www.w3.org/2000/svg">
+    <path id="motion-path"
+          d="M55 242L3 146L95 118L184 58C199.167 20.6028 234.5 -35.5314 254.5 39.1096C279.5 132.411 215.245 265.51 323 157.755C409.204 71.551 472.918 75.3333 494 88L516 191L342 235C324.05 272.526 308.735 342.063 391.075 320C494 292.421 575.5 330.083 575.5 224.5C575.5 118.917 642.245 2.5101 642.245 157.755"
+          stroke="black" stroke-width="4" stroke-linecap="round" />
+  </svg>
+
   <div class="rectangle"></div>
 </x-layout>
